2eff5fe29bb568615686af839e79f060aa235c94
Robots-Pixels <robotspixels@gmail.com>
2024-12-15 19:48:40 +0100

    Se connecter à la base de donnees MongoDB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config()
 const app = express();
 
-app.listen(4000, () => {
-    console.log("Server running on port 4000...")
-})
+mongoose.connect(
+    process.env.MONGO
+).then(
+    () => {
+        console.log("Connecté à MongoDB")
+        app.listen(4000, () => {
+            console.log("Server running on port 4000...")
+        })
+    }
+)
+.catch(
+    (error) => {
+        console.error(error);
+    }
+)
 
 app.get("/", (req, res) => {
     console.log("Connecté")
