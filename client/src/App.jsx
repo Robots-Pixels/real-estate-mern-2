9737a294b7d8e327f6a1da1cf0a6d6caf2e8ae3e
Robots-Pixels <robotspixels@gmail.com>
2024-12-15 18:52:51 +0100

    Ajout des pages principales et création de routes prinipales avec react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import SignUp from './Pages/SignUp'
+import SignIn from './Pages/SignIn'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+
 
 export default function App() {
   return (
-    <div>App</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/sign-up' element={<SignUp/>}></Route>
+          <Route path='/sign-in' element={<SignIn/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/profile' element={<Profile/>}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
