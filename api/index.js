d7ae9108c5206f9ebaec4d0d72da7749617cf37f
Robots-Pixels <robotspixels@gmail.com>
2024-12-16 12:34:32 +0100

    Fonctionnalités de Sign Up

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
-    return res.statusCode(statusCode).json({
+    return res.status(statusCode).json({
         success: false,
         statusCode,
         message
