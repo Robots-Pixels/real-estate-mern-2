7bbe09b0385a9aadbdcb35f551c41dd4a405d875
Robots-Pixels <robotspixels@gmail.com>
2024-12-16 17:45:29 +0100

    Faire apparaitre le profile si l'utilsateur s'inscrit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './Pages/Home.jsx'
 import SignUp from './Pages/SignUp'
 import SignIn from './Pages/SignIn'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
 import Header from './Components/Header.jsx'
-import Home from './Pages/Home.jsx'
+import PrivateRoute  from './Components/PrivateRoute.jsx'
 
 
 export default function App() {
@@ -19,7 +20,11 @@ export default function App() {
           <Route path='/sign-up' element={<SignUp/>}></Route>
           <Route path='/sign-in' element={<SignIn/>}></Route>
           <Route path='/about' element={<About/>}></Route>
-          <Route path='/profile' element={<Profile/>}></Route>
+
+          <Route element={<PrivateRoute/>}>
+            <Route path='/profile' element={<Profile />}></Route>
+          </Route>
+           
         </Routes>
       </BrowserRouter>
   )
